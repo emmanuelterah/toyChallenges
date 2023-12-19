c8f9bb0de96e839d440030f36c8ca825b8d939be
emmanuelterah <emmanuelterah98@gmail.com>
2023-12-19 08:56:47 +0300

    Added changes based on comments

--- NetSalary.js
+++ NetSalary.js
@@ -1,18 +1,18 @@
-function calculatePayee(grossIncome) {
+function calculatePayee(grossIncome) { //created a function named calculatePayee 
     let payee = 0;
     return payee;
 }
 
 
 ///////
-function calculateNHIF(basicSalary) {
+function calculateNHIF(basicSalary) { // created a function named calculateNHIF that passes the "basic salary" as an argument
     let nhifDeduction = 0;
     return nhifDeduction;
 }
 
 
 ///////
-function calculateNSSF(basicSalary) {
+function calculateNSSF(basicSalary) { // created a function named calculateNSSF that passes the "basic salary" as an argument
     let nssfDeduction = 0;
     return nssfDeduction;
 }
@@ -20,20 +20,22 @@ function calculateNSSF(basicSalary) {
 
 ///////
 function calculateNetSalary() {
-    let basicSalary = parseFloat(document.getElementById("basicSalary").value);
-    let benefits = parseFloat(document.getElementById("benefits").value);
+    let basicSalary = parseFloat(document.getElementById("basicSalary").value); // created a variable named basicSalary that gets input value from ID
+    let benefits = parseFloat(document.getElementById("benefits").value); // created a variable named benefits that gets input value from ID
 
-    let grossSalary = basicSalary + benefits;
-
-
-    let payee = calculatePayee(grossSalary);
-
-    let nhif = calculateNHIF(basicSalary);
+    let grossSalary = basicSalary + benefits; // created a variable named grossSalary that outputs the added value of basicSalary and benefits
 
 
-    let nssf = calculateNSSF(basicSalary);
+    let payee = calculatePayee(grossSalary); // created a variable that is based on the function calculatePayee
 
-    let netSalary = grossSalary - (payee + nhif + nssf);
+    let nhif = calculateNHIF(basicSalary); // created a variable that is based on the function calculateNHIF
 
-    document.getElementById("result").innerHTML = "Net Salary: " + netSalary.toFixed(2);
+
+    let nssf = calculateNSSF(basicSalary); // created a variable that is based on the function calculateNSSF
+
+    let netSalary = grossSalary - (payee + nhif + nssf); //created a variable named netSalary that outputs the value of deductions based on the gross salary 
+
+    document.getElementById("result").innerHTML = "Net Salary: " + netSalary.toFixed(2); // gets value from ID and returns the Net Salary value
 }
+
+calculateNetSalary(); //outputs value
