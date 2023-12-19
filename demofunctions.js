19ca07d6874ba98dc7c6bc1e06948aa992b1de65
emmanuelterah <152377704+emmanuelterah@users.noreply.github.com>
2023-12-19 11:38:04 +0300

    Delete demofunctions.js

--- demofunctions.js
+++
@@ -1,9 +0,0 @@
-const fruits = ["Mangoes", "Apples", "Oranges"]
-fruits.forEach(fruitIteratorFn)
-
-function fruitIteratorFn(param1, param2, param3) {
-    console.log(param1);
-    console.log(param2);
-    console.log(param3);
-}
-
